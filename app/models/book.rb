48bce5dcf969aa76e99ad171307d9d50bab02b0d
ChikaLong <longb.0818@gmail.com>
2021-09-22 09:12:01 +0000

    [Add]投稿数

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ApplicationRecord
   scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
   scope :created_yesterday, -> {where(created_at: Time.zone.yesterday.all_day)}
   scope :created_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
-  scope :created_lastweek, -> {where(created_at: 13.day.ago.beginning_of_day...7.day.ago.end_of_day)}
+  scope :created_lastweek, -> {where(created_at: 13.day.ago.beginning_of_day..7.day.ago.end_of_day)}
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
