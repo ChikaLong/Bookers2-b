b729f3839d0155a7babebdba6cf732889a5af4ad
ChikaLong <longb.0818@gmail.com>
2021-09-22 08:46:06 +0000

    [Add]投稿数

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
     @books=@user.books
     @book=Book.new
+    @today_book=@books.created_today
+    @yesterday_book=@books.created_yesterday
+    @week_book=@books.created_week
+    @lastweek_book=@books.created_lastweek
   end
 
   def edit
